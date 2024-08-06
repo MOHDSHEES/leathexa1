b6918f871b0ba6d6b6fccb03bd87c49817ea0ba7
MOHDSHEES <mohd.shees100@gmail.com>
2024-08-07 01:12:09 +0530

    breakpoints swiper

--- src/app/components/homePage/bestSeller.js
+++ src/app/components/homePage/bestSeller.js
@@ -53,6 +53,28 @@ const BestSeller = () => {
               slidesPerView={4.2}
               // navigation
               pagination={{ clickable: true }}
+              breakpoints={{
+                // when window width is >= 320px
+                320: {
+                  slidesPerView: 1.2,
+                  spaceBetween: 20,
+                },
+                // when window width is >= 640px
+                640: {
+                  slidesPerView: 2.2,
+                  spaceBetween: 20,
+                },
+                // when window width is >= 768px
+                768: {
+                  slidesPerView: 3.2,
+                  spaceBetween: 30,
+                },
+                // when window width is >= 1024px
+                1024: {
+                  slidesPerView: 4.2,
+                  spaceBetween: 30,
+                },
+              }}
               //   onSwiper={(swiper) => console.log(swiper)}
               //   onSlideChange={() => console.log("slide change")}
             >
